1346f167372eba8143f16d96eb96ced035b8189f
Joe-Becque <38461467+Joe-Becque@users.noreply.github.com>
2018-09-20 16:06:48 +0100

    Update comments

--- Constants.py
+++ Constants.py
@@ -1,14 +1,14 @@
 '''Physical Constants'''
-theta_i = 0                        #angle of incidence
-h = 6.63e-34                       #planck's const  (J s)
-hbar = 1.05e-34                    #planck's const  (J s)
-c = 3e8                            #speed of light in a vac  (m /s)
-q = 1.60218e-19                    #unit charge (C)
-n_air = 1                          #refractive index of air    ***CAN BE A FUNCTION OF E***
-T = 300                            #temp (K)
-k = 1.38064852e-23                 # boltzmann const ( m^2 kg s^-2 K^-1)
-A = 1e-4                           # Cross sectional area of solar cell (m^2)
-e = 8.854187817620e-12             # Vacuum permittivity (F⋅m−1)
+theta_i = 0                        #Angle of incidence
+h = 6.63e-34                       #Planck's const  (J s)
+hbar = 1.05e-34                    #Planck's const  (J s)
+c = 3e8                            #Speed of light in a vac  (m /s)
+q = 1.60218e-19                    #Unit charge (C)
+n_air = 1                          #Refractive index of air
+T = 300                            #Temp (K)
+k = 1.38064852e-23                 #Boltzmann const ( m^2 kg s^-2 K^-1)
+A = 1e-4                           #Cross sectional area of solar cell (m^2)
+e = 8.854187817620e-12             #Vacuum permittivity (F⋅m−1)
 
 
 
